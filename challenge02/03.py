e28dbf69ef2241d1e74a4fbd125eacb31c2b490c
ignacioFabiana <faby.tbe@gmail.com>
2024-07-29 17:11:30 -0300

    add challenge02/03.py

---
+++ challenge02/03.py
@@ -0,0 +1,18 @@
+''' Faça um programa que peça uma nota, entre zero e dez. 
+Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.**'''
+
+
+def request_grade():
+    while True:
+        try:
+            grade = float(input('Por favor insira uma nota com o valor entra 0 e 10: '))
+            if(0 <= grade <= 10):
+                print(f'Sua nota inserida foi : {grade}')
+                break
+            else:
+                ('Por favor, informe um valor válido entra 0 e 10')
+        except ValueError as err:
+            print(f'Entrada inválida. Por favor, insira um número. Detalhes: {err} ')    
+
+
+request_grade()            
