30b024ed218a65cd1c87f2c3b2318377fc6a54bf
ignacioFabiana <faby.tbe@gmail.com>
2024-08-01 10:12:15 -0300

    add 02-decision_making_and_loop_dtructures/01.py

--- 02-decision_making_and_loop_dtructures/01.py
+++ 02-decision_making_and_loop_dtructures/01.py
@@ -1,1 +1,16 @@
-# ex:01 desafio 02
+'''Faça um Programa que peça dois números e imprima o maior deles'''
+
+def check_larger_number():
+    try:
+        number01 = float(input('Por favor insira o primeiro número: '))
+        number02 = float(input('Por favor insira o segundo número: '))
+        if number01 > number02:
+            print(f'{number01} é o maior valor inserido!')
+        elif number01 < number02:
+            print(f'{number02} é o maior valor inserido')
+        else:
+            print('Os valores inseridos são iguais')
+    except ValueError as err:
+        print(f'Entrada inválida! Detalhes: {err}')
+
+check_larger_number()
