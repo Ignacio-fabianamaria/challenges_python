17d3a0c181d9d8590451d574516fb329a603e32c
Ignacio-fabianamaria <faby.tbe@gmail.com>
2024-08-15 13:05:54 -0300

    add 03-lists_tuples_and_dictionaries/05.py

---
+++ 03-lists_tuples_and_dictionaries/05.py
@@ -0,0 +1,9 @@
+"""Crie duas tuplas. Concatene-as para formar uma nova tupla."""
+
+tuple01 = (1, 3, 5, 7, 9)
+tuple02 = (2, 4, 6, 8, 10)
+concatenated_tuple = tuple01 + tuple02
+
+print(f'Tupla 01 {tuple01}')
+print(f'Tupla 02 {tuple02}\n')
+print(f'Tupla concatenada {concatenated_tuple}')
